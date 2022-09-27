5dcaad1fecdc60ffd74fa39be8499d33c55f59d5
YHatahet <yamen.hatahet@gmail.com>
2022-09-27 22:19:00 +0400

    Properly set up Hotel Model

--- Models/Hotel.cs
+++ Models/Hotel.cs
@@ -2,21 +2,21 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 
-namespace BookingAppApi.Models;
-
+namespace BookingApp.Models;
+[BsonIgnoreExtraElements]
 public class Hotel
 {
     [BsonId] //primary key
     [BsonRepresentation(BsonType.ObjectId)]
-
     public string _id { get; set; }
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string _owner { get; set; }
     public string name { get; set; }
-    public string _owner { get; set; } //TODO
-    public int rating { get; set; }
-    public int numOfRatings { get; set; }
     public string description { get; set; }
     public string address { get; set; }
     public string city { get; set; }
-    public int distanceFromCenter { get; set; }
-    public bool featured { get; set; }
+    public double distanceFromCenter { get; set; }
+    public double? rating { get; set; } = 0;
+    public int? numOfRatings { get; set; } = 0;
+    public bool? featured { get; set; } = false;
 }
