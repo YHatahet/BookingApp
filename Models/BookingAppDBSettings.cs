71ffbe49918f98c0706650d7c4c6d5d121558f76
YHatahet <yamen.hatahet@gmail.com>
2022-10-02 20:57:38 +0400

    Added token

--- Models/BookingAppDBSettings.cs
+++ Models/BookingAppDBSettings.cs
@@ -3,11 +3,10 @@ namespace BookingApp.Models;
 public class BookingAppDBSettings
 {
     public string ConnectionString { get; set; } = String.Empty;
-
     public string DatabaseName { get; set; } = String.Empty;
-
     public string HotelCollectionName { get; set; } = String.Empty;
     public string RoomCollectionName { get; set; } = String.Empty;
     public string UserCollectionName { get; set; } = String.Empty;
     public string ReviewCollectionName { get; set; } = String.Empty;
+    public string Token { get; set; } = String.Empty;
 }
