ff8cedc0c973dbaaea30485e6a8ed6717b7f502e
YHatahet <yamen.hatahet@gmail.com>
2022-10-02 16:22:43 +0400

    Populated class

--- Models/BookingAppDBSettings.cs
+++ Models/BookingAppDBSettings.cs
@@ -1,4 +1,4 @@
-namespace BookingAppApi.Models;
+namespace BookingApp.Models;
 
 public class BookingAppDBSettings
 {
@@ -7,7 +7,7 @@ public class BookingAppDBSettings
     public string DatabaseName { get; set; } = String.Empty;
 
     public string HotelCollectionName { get; set; } = String.Empty;
-    public string RoomsCollectionName { get; set; } = String.Empty;
-    public string UsersCollectionName { get; set; } = String.Empty;
-    public string ReviewsCollectionName { get; set; } = String.Empty;
+    public string RoomCollectionName { get; set; } = String.Empty;
+    public string UserCollectionName { get; set; } = String.Empty;
+    public string ReviewCollectionName { get; set; } = String.Empty;
 }
