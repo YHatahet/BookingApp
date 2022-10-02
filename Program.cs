c2511a388b165d6c83e3b3202cd9d90bc8da17ec
YHatahet <yamen.hatahet@gmail.com>
2022-10-02 16:39:14 +0400

    Cleanup

--- Program.cs
+++ Program.cs
@@ -1,33 +1,36 @@
 using MongoDB.Driver;
+using BookingApp.Models;
+using BookingApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Import from appsettings
-var BookingAppDBSettings = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("BookingAppDBSettings");
+// fetch config from app settings
+builder.Services.Configure<BookingAppDBSettings>(builder.Configuration.GetSection("BookingAppDBSettings"));
+builder.Services.AddSingleton<BookingAppService>();
 
-// Add services to the container.
-builder.Services.AddSingleton<IMongoClient, MongoClient>(s => new MongoClient(BookingAppDBSettings["ConnectionString"]));
+
+builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-// builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen();
 
-builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
     app.UseSwagger();
-    // app.UseSwaggerUI();
+    app.UseSwaggerUI();
 }
 
-app.UseStaticFiles();
+app.UseHttpsRedirection();
 
-app.UseRouting();
+app.MapControllers();
 
-app.UseAuthorization();
+// app.UseStaticFiles();
+
+// app.UseRouting();
+
+// app.UseAuthorization();
 
 app.Run();
