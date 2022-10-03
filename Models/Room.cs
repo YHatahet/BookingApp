fd522e2b124c7a2f0fc55cad53ae10e8bc285faa
YHatahet <yamen.hatahet@gmail.com>
2022-10-03 21:27:43 +0400

    Made room model arguments optional

--- Models/Room.cs
+++ Models/Room.cs
@@ -9,12 +9,12 @@ public class Room
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string? _id { get; set; }
-    public string title { get; set; } //TODO make unique
+    public string? title { get; set; } //TODO make unique
     [BsonRepresentation(BsonType.ObjectId)]
     public string? _hotel { get; set; }
-    public string description { get; set; }
-    public int maxTenants { get; set; }
-    public int pricePerNight { get; set; }
+    public string? description { get; set; }
+    public int? maxTenants { get; set; }
+    public int? pricePerNight { get; set; }
     public string[]? facilites { get; set; } = null; //TODO
     // public [{int number, int occupiedDates}] rooms { get; set; } //TODO
 }
