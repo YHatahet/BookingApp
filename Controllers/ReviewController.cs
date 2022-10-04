f64bf9d582bbddf2526ed42e9b975ee30df23d70
YHatahet <yamen.hatahet@gmail.com>
2022-10-04 09:12:38 +0400

    WIP added Review controller

--- Controllers/ReviewController.cs
+++ Controllers/ReviewController.cs
@@ -5,23 +5,60 @@ using BookingApp.Models;
 
 using MongoDB.Bson;
 using MongoDB.Driver;
-
+using BookingApp.Services;
 namespace BookingApp.Controllers;
 
 [ApiController, Route("reviews")]
-public class ReviewController  : ControllerBase
+public class ReviewController : ControllerBase
 {
 
-    private IMongoCollection<Hotel> _hotelCollection;
-    public ReviewController(IMongoClient client)
+    private IMongoCollection<Review> _reviewCollection;
+    public ReviewController(BookingAppService bookingAppService)
     {
-        var db = client.GetDatabase("hotelCollection");
-        _hotelCollection = db.GetCollection<Hotel>("hotels");
+        _reviewCollection = bookingAppService._reviewCollection;
     }
 
-    [HttpGet]
-    public IEnumerable<Hotel> Get()
+    [HttpPost("create/{hotelid}")]
+    public async Task<IActionResult> CreateReview(string hotelid, [FromBody] Review newReview)
     {
-        return _hotelCollection.Find(_ => true).ToList();
+        //TODO
+        await _reviewCollection.InsertOneAsync(newReview);
+        return CreatedAtAction(nameof(CreateReview), new { id = newReview._id }, newReview);
+
+    }
+
+    [HttpGet("all/{page}/{limit}")]
+    public async Task<List<Review>> GetAllReviews(int page, int limit)
+    {
+        return await _reviewCollection.Find(new BsonDocument()).Skip(page * limit).Limit(limit).ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<Review> GetReview(string id)
+    {
+        return await _reviewCollection.Find(o => o._id == id).FirstOrDefaultAsync();
+    }
+
+    //TODO get hotel reviews
+
+    [HttpPut("{id}")]
+    public async Task<Review> UpdateReview(string id, [FromBody] Review reviewBody)
+    {
+        var foundReview = await _reviewCollection.Find(o => o._id == id).FirstOrDefaultAsync();
+        if (reviewBody.rating != null) foundReview.rating = reviewBody.rating;
+        if (reviewBody.review != null) foundReview.review = reviewBody.review;
+        if (reviewBody._hotel != null) foundReview._hotel = reviewBody._hotel;
+        if (reviewBody._user != null) foundReview._user = reviewBody._user;
+        await _reviewCollection.ReplaceOneAsync(o => o._id == id, foundReview);
+        return foundReview;
+    }
+
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteReview(string id)
+    {
+        FilterDefinition<Review> filter = Builders<Review>.Filter.Eq("_id", id);
+        await _reviewCollection.DeleteOneAsync(filter);
+        return NoContent();
     }
 }
