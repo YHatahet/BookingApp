29f7aef41e3bc0adb8a8391294c3028b120bd03e
YHatahet <yamen.hatahet@gmail.com>
2022-10-04 09:16:18 +0400

    WIP added user controller code

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,42 +1,55 @@
-// using Microsoft.Extensions.Configuration;
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using BookingApp.Models;
 
 using MongoDB.Bson;
 using MongoDB.Driver;
-
+using BookingApp.Services;
 namespace BookingApp.Controllers;
 
 [ApiController, Route("users")]
 public class UserController : ControllerBase
 {
 
-    private IMongoCollection<User> _userCollection;
+    private readonly IMongoCollection<User> _userCollection;
 
-    public UserController(IMongoClient client)
+    public UserController(BookingAppService bookingAppService)
     {
-        var db = client.GetDatabase("hotelCollection");
-        _userCollection = db.GetCollection<User>("users");
+        _userCollection = bookingAppService._userCollection;
     }
 
 
-    [HttpPost, Route("register")]
-    public User RegisterNewUser()
+    [HttpGet, Route("all/{page}/{limit}")]
+    public async Task<List<User>> GetAllUsers(int page, int limit)
     {
-        // return _userCollection.Find(_ => true).ToList();
+        return await _userCollection.Find(new BsonDocument()).Skip(page * limit).Limit(limit).ToListAsync();
     }
 
 
-    [HttpPost, Route("login")]
-    public User Login()
+    [HttpGet("{id}")]
+    public async Task<User> GetUser(string id)
     {
-        // return _userCollection.Find(_ => true).ToList();
+        return await _userCollection.Find(o => o._id == id).FirstOrDefaultAsync();
     }
 
-    [HttpGet]
-    public IEnumerable<User> Get()
+    [HttpPut("{id}")]
+    public async Task<User> UpdateUser(string id, [FromBody] User userBody)
     {
-        return _userCollection.Find(_ => true).ToList();
+        //TODO refresh token , rehash pass
+        var foundUser = await _userCollection.Find(o => o._id == id).FirstOrDefaultAsync();
+        if (userBody.username != null) foundUser.username = userBody.username;
+        if (userBody.email != null) foundUser.email = userBody.email;
+        if (userBody.password != null) foundUser.password = userBody.password;
+        if (userBody.role != null) foundUser.role = userBody.role;
+        await _userCollection.ReplaceOneAsync(o => o._id == id, foundUser);
+        return foundUser;
+    }
+
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(string id)
+    {
+        FilterDefinition<User> filter = Builders<User>.Filter.Eq("_id", id);
+        await _userCollection.DeleteOneAsync(filter);
+        return NoContent();
     }
 }
