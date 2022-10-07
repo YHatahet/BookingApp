29324b196d4eeb2c2ad43021cb48d923ce30f650
YHatahet <yamen.hatahet@gmail.com>
2022-10-07 21:43:57 +0400

    Updated Room Model

--- Models/Room.cs
+++ Models/Room.cs
@@ -1,33 +1,42 @@
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+namespace BookingApp.Models;
 
-public struct ReservationType
+public class ReservationType
 {
     public DateTime start;
     public DateTime end;
 }
 
-
-public struct RoomType
+public class RoomEntry
 {
     public int? roomNumber;
-    public ReservationType[]? occupiedDates;
+    public List<ReservationType>? occupiedDates;
 }
 
-namespace BookingApp.Models
+
+public class Room
 {
-    public class Room
-    {
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string? _id { get; set; }
-        public string? title { get; set; }
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string? _hotel { get; set; }
-        public string? description { get; set; }
-        public int? maxTenants { get; set; }
-        public double? pricePerNight { get; set; }
-        public string[]? facilites { get; set; } = null;
-        public RoomType[] rooms { get; set; }
-    }
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? _id { get; set; }
+    public string? title { get; set; }
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? _hotel { get; set; }
+    public string? description { get; set; }
+    public int? maxTenants { get; set; }
+    public double? pricePerNight { get; set; }
+    public string[]? facilities { get; set; } = null;
+    public List<RoomEntry>? rooms { get; set; }
+}
+
+
+public class BookRoomBody
+{
+
+    public int? roomNumber { get; set; }
+    public string? startDate { get; set; }
+    public string? endDate { get; set; }
+    public int? numOfTenants { get; set; }
+
 }
