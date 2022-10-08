2dd67dfeddeca606f85464377b7816b414c9faf0
YHatahet <yamen.hatahet@gmail.com>
2022-10-08 13:24:21 +0400

    Added and injected token service

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 // fetch config from app settings
 builder.Services.Configure<BookingAppDBSettings>(builder.Configuration.GetSection("BookingAppDBSettings"));
 builder.Services.AddSingleton<BookingAppService>();
+builder.Services.AddSingleton<TokenService>();
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
