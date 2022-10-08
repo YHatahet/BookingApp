3bc0a47ca6642b38c9cf4bb4f6ed7ab3bd05c65a
YHatahet <yamen.hatahet@gmail.com>
2022-10-08 11:57:30 +0400

    Minor cleanup

--- Models/User.cs
+++ Models/User.cs
@@ -9,8 +9,8 @@ public class User
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string? _id { get; set; }
-    public string? username { get; set; } //TODO make unique
-    public string? email { get; set; } //TODO make unique
+    public string? username { get; set; }
+    public string? email { get; set; }
     public string? password { get; set; }
-    public string? role { get; set; } = "user";
+    public string? role { get; set; } = "user"; //user or admin. Defaults to user role
 }
